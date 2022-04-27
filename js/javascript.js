af1ac599f8f479188821e3175d0152d49cffe2a2
Shivesh Gupta <shiveshanjaligupta@gmail.com>
2022-04-27 22:20:14 +0530

    Navbar Implementation
    FullScreen Navbar

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("openbtn").style.visibility="hidden";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("openbtn").style.visibility="visible";
+  }
